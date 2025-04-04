4400c9b69974bd445df96435596105979f016a22
Rulo77 <ragome1234567@gmail.com>
2025-04-03 23:01:27 -0600

    add maps a seccion de sobre nosotros

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [GoogleMap, MapMarker],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  center: google.maps.LatLngLiteral = {
+    lat: 28.687123613229357, // Latitud de tu ubicación
+    lng: -106.13087525581909,  // Longitud de tu ubicación
+  };
+  zoom = 15;
+  mapOptions: google.maps.MapOptions = {
+    disableDefaultUI: true,
+    zoomControl: true
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 }
