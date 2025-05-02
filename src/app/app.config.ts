c529363d61e05e452b01d22ab153294533803a5c
Rulo77 <ragome1234567@gmail.com>
2025-05-01 19:24:31 -0600

    mejoras en la navegacion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection  } from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withInMemoryScrolling({
+    scrollPositionRestoration: 'disabled', // 'disabled' | 'enabled' | 'top'
+    anchorScrolling: 'disabled',           // 'enabled' | 'disabled'
+  }))]
 };
--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -1,17 +1,56 @@
-import { Component, HostListener } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { Location } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+
+  currentFragment: string | null = null;
+  currentPath: string = '';
+
+  private router = inject(Router);
+  private location = inject(Location);
+
   isMenuOpen = false;
   isScrolled = false;
+  isContactoVisible = false;
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      // Scroll suave al top
+      const fragment = this.router.parseUrl(this.router.url).fragment;
+      const urlTree = this.router.parseUrl(this.router.url);
+      this.currentFragment = fragment;
+      this.currentPath = urlTree.root.children['primary']?.segments.map(s => s.path).join('/') || '';
+
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    });
+  }
+
+  ngAfterViewInit(): void {
+    const contacto = document.getElementById('contacto');
+    if (contacto) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          this.isContactoVisible = entries[0].isIntersecting;
+        },
+        {
+          threshold: 0.5, // al menos 50% del footer visible
+        }
+      );
+      observer.observe(contacto);
+    }
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
@@ -19,14 +58,30 @@ export class NavBarComponent {
   }
 
   isActive(route: string): boolean {
-    return this.router.url === route;
+    return this.currentPath === route && !this.isContactoVisible;
   }
-  navigateTo(route: string): void {
-    this.router.navigate([route]);
+
+  navigateTo(route: string, fragment?: string): void {
+    this.router.navigate([route], { fragment: fragment });
     this.isMenuOpen = false;
   }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  isActiveInicio(): boolean {
+    return this.currentPath === '' && !this.currentFragment;
+  }
+
+
+  navigateToContacto(): void {
+    this.location.replaceState(this.router.url.split('#')[0] + '#contacto');
+    setTimeout(() => {
+      const element = document.getElementById('contacto');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 50);
   }
 }
